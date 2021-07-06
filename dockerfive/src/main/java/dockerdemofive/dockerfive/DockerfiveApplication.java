4472a00615d84c15899d0f4bff05dd2b9691b09e
Rishi7pillai <hrushikeshpollai777@gmail.com>
2021-07-07 00:31:10 +0530

    first

---
+++ dockerfive/src/main/java/dockerdemofive/dockerfive/DockerfiveApplication.java
@@ -0,0 +1,13 @@
+package dockerdemofive.dockerfive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerfiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerfiveApplication.class, args);
+	}
+
+}
---
+++ dockerfive/src/test/java/dockerdemofive/dockerfive/DockerfiveApplicationTests.java
@@ -0,0 +1,13 @@
+package dockerdemofive.dockerfive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerfiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
